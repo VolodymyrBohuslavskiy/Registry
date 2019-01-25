340d08e5dc6fc1ed955a2a4ce2454c09ba17cf00
VolodymyrBohuslavskiy <936187435bv@gmail.com>
2019-01-26 01:07:36 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,42 @@ import {NgForm} from '@angular/forms';
 export class AppComponent {
   title = 'Task-1';
 
+  reg = false;
+  log = false;
+
   user = {username: null, email: null, password: null};
+
+  clearFields() {
+    this.user.username = null;
+    this.user.password = null;
+    this.user.email = null;
+  }
+
+  hide() {
+    this.log = false;
+    this.reg = false;
+  }
+
+  onLog() {
+    this.log = true;
+    this.reg = false;
+  }
+
+  onReg() {
+    this.reg = true;
+    this.log = false;
+  }
 
   registry(form: NgForm) {
     this.user = form.value;
     console.log(this.user);
+    this.hide();
+    form.reset();
   }
 
   login() {
     console.log(this.user);
+    this.hide();
   }
 
 }
